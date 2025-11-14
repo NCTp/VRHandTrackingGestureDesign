c5cf7122ff28bbd6422d613c43ca798ad12ab1c0
NCTP <cjfgh530@naver.com>
2025-11-14 11:14:07 +0900

    Custom Hand Pose Set

---
+++ Assets/HandPoseTest.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class HandPoseTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DebugLeftHandThumbsUp()
+    {
+        Debug.LogWarning("Left Thumbs Up");
+    }
+    public void DebugLeftHandThumbsDown()
+    {
+        Debug.LogWarning("Left Thumbs Down");
+    }
+    public void DebugRightHandThumbsUp()
+    {
+        Debug.LogWarning("Right Thumbs Up");
+    }
+    public void DebugRightHandThumbsDown()
+    {
+        Debug.LogWarning("Right Thumbs Down");
+    }
+}
